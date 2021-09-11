64a00bed48405141706ad937fd7f86cd99976d17
Tracy G <tgill009@odu.edu>
2021-09-11 14:31:31 -0400

    Tweak code to use tuples and tuple unpacking

--- newtons_method.py
+++ newtons_method.py
@@ -1,47 +1,4 @@
-#! /usr/bin/env python3
-
-import sys
-
-from typing import (Callable)
-import typing
-from fractions import (Fraction)
-
-
-EPSILON = 10e-6
-MAX_ITERATIONS = 100
-
-def newtons_method(f, df, x_n, eps=EPSILON):
-
-    n = 0
-
-    next_x_n = x_n + 1000 * eps
-    while abs(x_n - next_x_n) > eps:
-        x_n = next_x_n
-        next_x_n = x_n - (f(x_n) / df(x_n))
-
-        if n >= MAX_ITERATIONS:
-            break
-
-        n += 1
-
-    return x_n
-
-
-def main():
-    try:
-        initial_guess = float(sys.argv[1])
-
-    except IndexError as error:
-        print("Usage: {0} initial_guess".format(*sys.argv))
-        sys.exit(1)
-
-    except ValueError as error:
-        print("ERROR {0} is not a valid number".format(*sys.argv))
-        print("  " + str(error))
-        sys.exit(2)
-
-    # Function (f) and its derivative (dx)
-    def f(x):
+  def f(x):
         return (x ** 2) - 1
 
     def df(x):
@@ -59,3 +16,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
